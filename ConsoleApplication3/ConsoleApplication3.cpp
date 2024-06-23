43e9604087f6d1db7aabf134bd7adede417120f3
dbere <dberezhkov2016@gmail.com>
2024-06-23 18:00:23 +0300

    Correction update!

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -7,71 +7,106 @@ private:
 	double denominator;
 public:
 	Fraction() {
-		numerator = NULL;
+		numerator = 0;
 		denominator = 1;
 	}
-
-	void MultyFraction(double numer1, double denomin1, double numer2, double denomin2) {
-		if (denomin1 == 0 || denomin2 == 0) {
-			std::cout << "You can't divide by zero! Error!!!" << std::endl;
+	Fraction(double n, double d) {
+		numerator = n;
+		if (d == 0) {
+			std::cout << "error!" << std::endl;
 			return;
 		}
-		numerator = numer1 * numer2;
-		denominator = denomin1 * denomin2;
-	}
-
-	void DevideFraction(double numer1, double denomin1, double numer2, double denomin2) {
-		if (denomin1 == 0 || denomin2 == 0) {
-			std::cout << "You can't divide by zero! Error!!!" << std::endl;
-			return;
+		else
+		{
+			denominator = d;
 		}
-		numerator = numer1 * denomin2;
-		denominator = denomin1 * numer2;
 	}
 
-	void SumFraction(double numer1, double denomin1, double numer2, double denomin2) {
-		if (denomin1 == 0 || denomin2 == 0) {
+	Fraction MultyFraction(Fraction obj) {
+		if (obj.denominator == 0 || denominator == 0) {
 			std::cout << "You can't divide by zero! Error!!!" << std::endl;
-			return;
+			return Fraction();
 		}
-		denominator = denomin1 * denomin2;
-		double new_numerator1 = numer1 * denomin2;
-		double new_numerator2 = numer2 * denomin1;
-		numerator = new_numerator1 + new_numerator2;
+		double tmp_numerator = obj.numerator * numerator;
+		double tmp_denominator = obj.denominator * denominator;
+
+		Fraction result(tmp_numerator, tmp_denominator);
+
+		return result;
 	}
 
-	void MinusFraction(double numer1, double denomin1, double numer2, double denomin2) {
-		if (denomin1 == 0 || denomin2 == 0) {
+	Fraction DevideFraction(Fraction obj) {
+		if (obj.denominator == 0 || denominator == 0) {
 			std::cout << "You can't divide by zero! Error!!!" << std::endl;
-			return;
+			return Fraction();
 		}
-		denominator = denomin1 * denomin2;
-		double new_numerator1 = numer1 * denomin2;
-		double new_numerator2 = numer2 * denomin1;
-		numerator = new_numerator1 - new_numerator2;
+		double tmp_numerator = numerator * obj.denominator;
+		double tmp_denominator = denominator * obj.numerator;
+
+		Fraction result(tmp_numerator, tmp_denominator);
+
+		return result;
 	}
 
+	Fraction SumFraction(Fraction obj) {
+		if (obj.denominator == 0 || denominator == 0) {
+			std::cout << "You can't divide by zero! Error!!!" << std::endl;
+			return Fraction();
+		}
+		double common_denominator = obj.denominator * denominator;
+		double new_numerator1 = numerator * obj.denominator;
+		double new_numerator2 = obj.numerator * denominator;
+
+		double tmp_numerator = new_numerator1 + new_numerator2;
+		double tmp_denominator = common_denominator;
+
+		Fraction result(tmp_numerator, tmp_denominator);
+
+		return result;
+	}
+
+	Fraction MinusFraction(Fraction obj) {
+		if (obj.denominator == 0 || denominator == 0) {
+			std::cout << "You can't divide by zero! Error!!!" << std::endl;
+			return Fraction();
+		}
+		double common_denominator = obj.denominator * denominator;
+		double new_numerator1 = numerator * obj.denominator;
+		double new_numerator2 = obj.numerator * denominator;\
+
+		double tmp_numerator = new_numerator1 - new_numerator2;
+		double tmp_denominator = common_denominator;
+
+		Fraction result(tmp_numerator, tmp_denominator);
+
+		return result;
+	}
+	
 	void PrintResult() {
-		std::cout << "Result is: " << numerator / denominator << std::endl;
+		std::cout << "Result is: " << numerator << "/" << denominator << " or " << numerator / denominator << std::endl;
 	}
 
 };
 
 int main()
 {
-	Fraction fraction1;
+	Fraction fraction1(1,2);
 
-	//fraction1.MultyFraction(1,3,2,3);
-	//fraction1.PrintResult();
-	
-	fraction1.DevideFraction(1, 0, 2, 3);
-	fraction1.PrintResult();
+	Fraction fraction2(1,2);
 
-	//fraction1.SumFraction(1, 3, 2, 3);
-	//fraction1.PrintResult();
-	
-	//fraction1.MinusFraction(1, 3, 2, 3);
-	//fraction1.PrintResult();
+	Fraction fraction3;
+
+	fraction3 = fraction2.MultyFraction(fraction1);
+	fraction3.PrintResult();
+
+	fraction3 = fraction2.DevideFraction(fraction1);
+	fraction3.PrintResult();
+
+	fraction3 = fraction2.SumFraction(fraction1);
+	fraction3.PrintResult();
+
+	fraction3 = fraction2.MinusFraction(fraction1);
+	fraction3.PrintResult();
 
 }
 
